87bc0e04620457543adf51c59fe477ddbe0c56e2
Jadon Calvert <calvertjadon@gmail.com>
2024-02-28 15:58:09 -0500

    add bash aliases and better install script

--- setup.sh
+++ setup.sh
@@ -1,14 +1,41 @@
 #!/usr/bin/env bash
 
-
-# install nodejs
-curl -fsSL https://deb.nodesource.com/setup_21.x | sudo -E bash - &&\
-sudo apt install -y nodejs
+# install nodejs repo
+if ! command -v npm >/dev/null 2>&1; then
+	curl -fsSL https://deb.nodesource.com/setup_21.x | sudo -E bash -
+fi
 
 # install other deps
-sudo apt install -y git stow ripgrep fd-find fzf
+# name,cmd
+required=("ripgrep,rg" "fzf,fzf" "git,git" "stow,stow" "fd-find,fd" "npm,npm")
+missing=()
+
+for i in "${required[@]}"; do
+	name=${i%,*}
+	cmd=${i#*,}
+
+	if ! command -v "$cmd" >/dev/null 2>&1; then
+		echo "$name is missing"
+		missing+=("$cmd")
+	fi
+done
+
+if [ ${#missing[@]} -gt 0 ]; then
+	echo "Installing missing dependencies:" "${missing[@]}"
+	sudo apt install -y "${missing[@]}"
+fi
 
 git submodule init
 git submodule update
 
 stow .
+
+case "$HOSTNAME" in
+"L1IT-LP01") ;;
+*) ;;
+esac
+
+echo 'alias ll="ls -al"' >>~/.bashrc
+
+source "$HOME/.bashrc"
+echo "Setup successful."

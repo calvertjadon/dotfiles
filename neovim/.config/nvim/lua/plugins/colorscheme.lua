aaeb1e468800e346951247f49682baba48a05a58
Jadon Calvert <calvertjadon@gmail.com>
2025-09-03 11:02:39 -0400

    add neovim config

---
+++ neovim/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+    },
+  },
+}
